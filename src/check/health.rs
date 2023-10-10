62129e5b7d255e05813fc42b3429512e4999961b
ticbh <dwhat@vip.qq.com>
2023-10-10 19:20:47 +0800

    添加健康检查注释

--- src/check/health.rs
+++ src/check/health.rs
@@ -13,29 +13,35 @@ lazy_static! {
     static ref HEALTH_CHECK: RwLock<HealthCheck> = RwLock::new(HealthCheck::new(60, 3, 2));
 }
 
+/// 每个SocketAddr的记录值
 struct HealthRecord {
+    /// 最后的记录时间
     last_record: Instant,
+    /// 失败的恢复时间
     fail_timeout: Duration,
+    /// 当前连续失败的次数
     fall_times: usize,
+    /// 当前连续成功的次数
     rise_times: usize,
-    status: u8,
+    /// 当前的状态
+    failed: bool,
 }
 
 impl HealthRecord {
-    pub fn new() -> Self {
+    pub fn new(fail_timeout: usize) -> Self {
         Self {
             last_record: Instant::now(),
-            fail_timeout: Duration::new(60, 0),
+            fail_timeout: Duration::new(fail_timeout as u64, 0),
             fall_times: 0,
             rise_times: 0,
-            status: 0,
+            failed: false,
         }
     }
 
     pub fn clear_status(&mut self) {
         self.fall_times = 0;
         self.rise_times = 0;
-        self.status = 0;
+        self.failed = false;
     }
 }
 
@@ -66,7 +72,8 @@ impl HealthCheck {
     }
 
     /// 检测状态是否能连接
-    pub fn is_falldown(addr: &SocketAddr) -> bool {
+    pub fn is_fall_down(addr: &SocketAddr) -> bool {
+        // 只读，获取读锁
         if let Ok(h) = HEALTH_CHECK.read() {
             if !h.health_map.contains_key(addr) {
                 return false;
@@ -75,58 +82,65 @@ impl HealthCheck {
             if Instant::now().duration_since(value.last_record) > value.fail_timeout {
                 return false;
             }
-            h.health_map[addr].status != 0
+            h.health_map[addr].failed
         } else {
             false
         }
     }
 
     /// 失败时调用
-    pub fn add_falldown(addr: SocketAddr) {
+    pub fn add_fall_down(addr: SocketAddr) {
+        // 需要写入，获取写入锁
         if let Ok(mut h) = HEALTH_CHECK.write() {
             if !h.health_map.contains_key(&addr) {
-                let mut health = HealthRecord::new();
+                let mut health = HealthRecord::new(h.fail_timeout);
                 health.fall_times = 1;
                 h.health_map.insert(addr, health);
             } else {
                 let max_fails = h.max_fails;
                 let value = h.health_map.get_mut(&addr).unwrap();
+                // 超出最大的失败时长，重新计算状态
                 if Instant::now().duration_since(value.last_record) > value.fail_timeout {
                     value.clear_status();
                 }
                 value.last_record = Instant::now();
                 value.fall_times += 1;
+                value.rise_times = 0;
 
                 if value.fall_times >= max_fails {
-                    value.status = 1;
+                    value.failed = true;
                 }
             }
         }
     }
 
     /// 成功时调用
-    pub fn add_riseup(addr: SocketAddr) {
+    pub fn add_rise_up(addr: SocketAddr) {
+        // 需要写入，获取写入锁
         if let Ok(mut h) = HEALTH_CHECK.write() {
             if !h.health_map.contains_key(&addr) {
-                let mut health = HealthRecord::new();
+                let mut health = HealthRecord::new(h.fail_timeout);
                 health.rise_times = 1;
                 h.health_map.insert(addr, health);
             } else {
                 let min_rises = h.min_rises;
                 let value = h.health_map.get_mut(&addr).unwrap();
+                // 超出最大的失败时长，重新计算状态
                 if Instant::now().duration_since(value.last_record) > value.fail_timeout {
                     value.clear_status();
                 }
                 value.last_record = Instant::now();
                 value.rise_times += 1;
+                value.fall_times = 0;
 
                 if value.rise_times >= min_rises {
-                    value.status = 0;
+                    value.failed = false;
                 }
             }
         }
     }
 
+    // 将TcpStream::connect函数替换成这个函数，将自动启用被动健康检查
     pub async fn connect<A>(addr: &A) -> io::Result<TcpStream>
     where
         A: ToSocketAddrs,
@@ -135,17 +149,18 @@ impl HealthCheck {
         let mut last_err = None;
 
         for addr in addrs {
-            if Self::is_falldown(&addr) {
+            // 健康检查失败，直接返回错误
+            if Self::is_fall_down(&addr) {
                 last_err = Some(io::Error::new(io::ErrorKind::Other, "health check falldown"));
             } else {
                 match TcpStream::connect(&addr).await {
                     Ok(stream) => 
                     {
-                        Self::add_riseup(addr);
+                        Self::add_rise_up(addr);
                         return Ok(stream)
                     },
                     Err(e) => {
-                        Self::add_falldown(addr);
+                        Self::add_fall_down(addr);
                         last_err = Some(e)
                     },
                 }
